9dd2e0a3f207bac4bf48c211893b86aaf7f45a48
Hugo Huipet <hugo.huipet@gmail.com>
2026-04-23 23:48:20 -0700

    Added orders page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router'
 import { HomePage } from './pages/HomePage'
 import './App.css'
 import {CheckoutPage} from "./pages/CheckoutPage.tsx";
+import {OrdersPage} from "./pages/OrdersPage.tsx";
 
 function App() {
 
@@ -10,6 +11,8 @@ function App() {
     <Routes>
       <Route index element={<HomePage />}/>
       <Route path="checkout" element={<CheckoutPage />}/>
+      <Route path="orders" element={<OrdersPage />}/>
+
     </Routes>
   )
 }
