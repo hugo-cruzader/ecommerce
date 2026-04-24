03ef2cff323eff4b26b2447b3881df8c41bd6da3
Hugo Huipet <hugo.huipet@gmail.com>
2026-04-24 00:39:06 -0700

    Added not found page

--- src/pages/notfound/NotFoundPage.tsx
+++ src/pages/notfound/NotFoundPage.tsx
@@ -1,10 +1,12 @@
 import {Header} from "../../components/Header.tsx";
+import './NotFoundPage.css'
+
 
 export function NotFoundPage() {
   return(
     <>
       <Header/>
-      <div className="orders-page">
+      <div className="not-found">
         <h1> 404 </h1>
         <h2>NOT FOUND</h2>
       </div>
